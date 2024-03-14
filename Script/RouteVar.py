073d2d5e592f5ac9e0f91bd33af17e07e52fb7c5
Bobonbush <tdadung23@apcs.fitus.edu.vn>
2024-03-14 16:10:43 +0700

    Update RouteVar and Var

---
+++ Script/RouteVar.py
@@ -0,0 +1,19 @@
+
+
+
+
+class RouteVar :
+
+    def __init__(self , RouteId, RouteVarId, RouteVarName,RouteVarShortName, RouteNo, StartStop, EndStop, Distance, Outbound, RunningTime):
+        self.RouteId = RouteId
+        self.RouteVarId = RouteVarId
+        self.RouteVarName = RouteVarName
+        self.RouteVarShortName = RouteVarShortName
+        self.RouteNo = RouteNo
+        self.StartStop = StartStop
+        self.EndStop = EndStop
+        self.Distance = Distance
+        self.Outbound = Outbound
+        self.RunningTime = RunningTime
+
+    
