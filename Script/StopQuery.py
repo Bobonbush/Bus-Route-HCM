6f4b890c9fe25ff7027c2d2f4d0d33d946e652bd
Bobonbush <tdadung23@apcs.fitus.edu.vn>
2024-03-17 20:00:23 +0700

    Gần như là end game

--- Script/StopQuery.py
+++ Script/StopQuery.py
@@ -45,6 +45,104 @@ class StopQuery :
         print(list.__len__())
         return list
     
+    def SearchByStopId(self, StopId):
+        list = []
+        for item in self.stops:
+            if item.StopId == StopId:
+                list.append(item)
+        return list
+    
+    def SearchByCode(self, Code):
+        list = []
+        for item in self.stops:
+            if item.Code == Code:
+                list.append(item)
+        return list
+    
+    def SearchByName(self, Name):
+        list = []
+        for item in self.stops:
+            if item.Name == Name:
+                list.append(item)
+        return list
+    
+    def SearchByStopType(self, StopType):
+        list = []
+        for item in self.stops:
+            if item.StopType == StopType:
+                list.append(item)
+        return list
+    
+    def SearchByZone(self, Zone):
+        list = []
+        for item in self.stops:
+            if item.Zone == Zone:
+                list.append(item)
+        return list
+    
+    def SearchByWard(self, Ward):
+        list = []
+        for item in self.stops:
+            if item.Ward == Ward:
+                list.append(item)
+        return list
+    
+    def SearchByAddressNo(self, AddressNo):
+        list = []
+        for item in self.stops:
+            if item.AddressNo == AddressNo:
+                list.append(item)
+        return list
+    
+    def SearchByStreet(self, Street):
+        list = []
+        for item in self.stops:
+            if item.Street == Street:
+                list.append(item)
+        return list
+    
+    def SearchBySupportDisability(self, SupportDisability):
+        list = []
+        for item in self.stops:
+            if item.SupportDisability == SupportDisability:
+                list.append(item)
+        return list
+    
+    def SearchByStatus(self, Status):
+        list = []
+        for item in self.stops:
+            if item.Status == Status:
+                list.append(item)
+        return list
+    
+    def SearchByLng(self, Lng):
+        list = []
+        for item in self.stops:
+            if item.Lng == Lng:
+                list.append(item)
+        return list
+    
+    def SearchByLat(self, Lat):
+        list = []
+        for item in self.stops:
+            if item.Lat == Lat:
+                list.append(item)
+        return list
+    
+    def SearchBySearch(self, Search):
+        list = []
+        for item in self.stops:
+            if item.Search == Search:
+                list.append(item)
+        return list
+    
+    def SearchByRoutes(self, Routes):
+        list = []
+        for item in self.stops:
+            if item.Routes == Routes:
+                list.append(item)
+        return list
+    
     def outputAsCSV(self, ans):
         writer = csv.DictWriter(StopQuery.csvfile, fieldnames=StopQuery.field)
         writer.writeheader()
